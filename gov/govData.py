743c3b88d9bc66dd0e3be279a131e6a17fb02685
Maciej Nasinski <nasinski.maciej@gmail.com>
2022-05-20 20:53:35 +0000

    style

--- gov/govData.py
+++ gov/govData.py
@@ -1,102 +1,114 @@
 # -*- coding: utf-8 -*-
 import os
+import zipfile
+import tempfile
+import glob
+import collections
+from govDataUtils.funs import get_plgov_cols, process_gov, list2gzip, get_url
+
 os.chdir("./gov")
 
-from govDataUtils.funs import *
-
-main_url = "https://arcgis.com/sharing/rest/content/items/"
+MAIN_URL = "https://arcgis.com/sharing/rest/content/items/"
 # Looks to be stable
-woj_sha = "a8c562ead9c54e13a135b02e0d875ffb"
-woj_sha_old = "b03b454aed9b4154ba50df4ba9e1143b"
-pow_sha = "e16df1fa98c2452783ec10b0aea4b341"
-all_vac_sha = "b860f2797f7f4da789cb6fccf6bd5bc7"
+WOJ_SHA = "a8c562ead9c54e13a135b02e0d875ffb"
+WOJ_SHA_OLD = "b03b454aed9b4154ba50df4ba9e1143b"
+POW_SHA = "e16df1fa98c2452783ec10b0aea4b341"
+ALL_VAC_SHA = "b860f2797f7f4da789cb6fccf6bd5bc7"
 
-pol_encoding = "Windows-1250"
-path_data = tempfile.mkdtemp()
+POL_ENCODING = "Windows-1250"
+PATH_DATA = tempfile.mkdtemp()
 
-path_res = "./data"
-path_res_pow1 = "./data/pow_df.csv.gz"
-path_res_pow2 = "./data/pow_df_full.csv.gz"
-path_res_woj = "./data/woj_df.csv.gz"
-path_res_pow_vac = "./data/pow_df_vac.csv.gz"
-path_res_woj_vac = "./data/woj_df_vac.csv.gz"
+ALL_ID = "cały kraj"
+WOJ_VAR = "wojewodztwo"
+POW_VAR = "powiat_miasto"
+
+PATH_RES = "./data"
+PATH_RES_POW1 = "./data/pow_df.csv.gz"
+PATH_RES_POW2 = "./data/pow_df_full.csv.gz"
+PATH_RES_WOJ = "./data/woj_df.csv.gz"
+PATH_RES_POW_VAC = "./data/pow_df_vac.csv.gz"
+PATH_RES_WOJ_VAC = "./data/woj_df_vac.csv.gz"
 
 try:   
-  os.remove(path_res_pow1)
-  os.remove(path_res_pow2)
-  os.remove(path_res_woj)
-  os.remove(path_res_pow_vac)
-  os.remove(path_res_woj_vac)
+    os.remove(PATH_RES_POW1)
+    os.remove(PATH_RES_POW2)
+    os.remove(PATH_RES_WOJ)
+    os.remove(PATH_RES_POW_VAC)
+    os.remove(PATH_RES_WOJ_VAC)
 except:
-  None
-  
-tempzip = tempfile.TemporaryFile(suffix = ".zip")
-all_id = "cały kraj"
-woj_var = "wojewodztwo"
-pow_var = "powiat_miasto"
+    None
 
 try:
-  os.mkdir(path_res)
+    os.mkdir(PATH_RES)
 except:
-  None
-  
-zip_path, _ = urllib.request.urlretrieve(main_url + pow_sha + "/data")
-with zipfile.ZipFile(zip_path, "r") as f:
-    f.extractall(path_data)
+    None
 
-zip_path, _ = urllib.request.urlretrieve(main_url + woj_sha + "/data")
+zip_path, _ = get_url(MAIN_URL + POW_SHA + "/data")
 with zipfile.ZipFile(zip_path, "r") as f:
-    f.extractall(path_data)
+    f.extractall(PATH_DATA)
 
-zip_path, _ = urllib.request.urlretrieve(main_url + all_vac_sha + "/data")
+zip_path, _ = get_url(MAIN_URL + WOJ_SHA + "/data")
 with zipfile.ZipFile(zip_path, "r") as f:
-    f.extractall(path_data)
+    f.extractall(PATH_DATA)
+
+zip_path, _ = get_url(MAIN_URL + ALL_VAC_SHA + "/data")
+with zipfile.ZipFile(zip_path, "r") as f:
+    f.extractall(PATH_DATA)
 
 # Powiat
-pow_files = sorted(glob.glob('{path_data}/*_pow_eksport.csv'.format(path_data = path_data)))
-headers = get_plgov_cols(pow_files, all_id)
+pow_files = sorted(glob.glob(f'{PATH_DATA}/*_pow_eksport.csv'))
+headers = get_plgov_cols(pow_files, ALL_ID)
 base_cols_p = ["wojewodztwo", "powiat_miasto", "liczba_przypadkow", "liczba_na_10_tys_mieszkancow",
 "zgony","stan_rekordu_na", "Date", "name"]
 cols_p = list(collections.OrderedDict().fromkeys(base_cols_p + headers).keys())
 pow_row = collections.namedtuple('pow_row', cols_p)
 
-pow_mapp = {"liczba_wszystkich_zakazen":"liczba_przypadkow","liczba_wszystkich_zakazen_na_10_tys_mieszkancow":"liczba_na_10_tys_mieszkancow", "powiat":"powiat_miasto"}
-res = process_gov(pow_files, ["20210107"], pow_mapp, pow_row, 1, all_id)
+pow_mapp = {"liczba_wszystkich_zakazen":"liczba_przypadkow",
+            "liczba_wszystkich_zakazen_na_10_tys_mieszkancow":"liczba_na_10_tys_mieszkancow",
+            "powiat":"powiat_miasto"}
+res = process_gov(pow_files, ["20210107"], pow_mapp, pow_row, 1, ALL_ID)
 
-list2gzip(path_res_pow1, res, cols_p, 7)
-list2gzip(path_res_pow2, res, cols_p, len(cols_p))
+list2gzip(PATH_RES_POW1, res, cols_p, 7)
+list2gzip(PATH_RES_POW2, res, cols_p, len(cols_p))
 
 # Wojewodztwo
-woj_files = sorted(glob.glob('{path_data}/*_woj_eksport.csv'.format(path_data = path_data)))
-headers = get_plgov_cols(woj_files, all_id)
+woj_files = sorted(glob.glob(f'{PATH_DATA}/*_woj_eksport.csv'))
+headers = get_plgov_cols(woj_files, ALL_ID)
 base_cols_w = ["wojewodztwo", "liczba_przypadkow", "liczba_na_10_tys_mieszkancow",
 "zgony","stan_rekordu_na", "Date", "name"]
 cols_w = list(collections.OrderedDict().fromkeys(base_cols_w + headers).keys())
 woj_row = collections.namedtuple('woj_row', cols_w)
-woj_mapp = {"liczba_wszystkich_zakazen":"liczba_przypadkow","liczba_wszystkich_zakazen_na_10_tys_mieszkancow":"liczba_na_10_tys_mieszkancow"}
+woj_mapp = {"liczba_wszystkich_zakazen":"liczba_przypadkow",
+            "liczba_wszystkich_zakazen_na_10_tys_mieszkancow":"liczba_na_10_tys_mieszkancow"}
 
-res = process_gov(woj_files, ["20210107"], woj_mapp, woj_row, 1, all_id)
+res = process_gov(woj_files, ["20210107"], woj_mapp, woj_row, 1, ALL_ID)
 
-list2gzip(path_res_woj, res, cols_w, len(cols_w))
+list2gzip(PATH_RES_WOJ, res, cols_w, len(cols_w))
 
 # Powiat Vac
-pow_vac_files = sorted(glob.glob('{path_data}/*_pow_szczepienia.csv'.format(path_data = path_data)))
-headers = get_plgov_cols(pow_vac_files, all_id)
-base_cols_p_v = [ 'wojewodztwo', 'powiat_miasto', 'liczba_szczepien_ogolem', 'liczba_szczepien_ogolnie', 'dawka_1_ogolem', 'dawka_2_ogolem', 'dawka_3_ogolem', 'dawka_1_dziennie', 'dawka_2_dziennie', 'dawka_3_dziennie', 'dawka_przypominajaca_dziennie', 'liczba_szczepien_dziennie', 'dawka_przypominajaca_ogolem', 'teryt', "name", "Date", "stan_rekordu_na"]
+pow_vac_files = sorted(glob.glob(f'{PATH_DATA}/*_pow_szczepienia.csv'))
+headers = get_plgov_cols(pow_vac_files, ALL_ID)
+base_cols_p_v = ['wojewodztwo', 'powiat_miasto', 'liczba_szczepien_ogolem', 'liczba_szczepien_ogolnie',
+                 'dawka_1_ogolem', 'dawka_2_ogolem', 'dawka_3_ogolem', 'dawka_1_dziennie', 'dawka_2_dziennie',
+                 'dawka_3_dziennie', 'dawka_przypominajaca_dziennie', 'liczba_szczepien_dziennie',
+                 'dawka_przypominajaca_ogolem', 'teryt', "name", "Date", "stan_rekordu_na"]
 cols_p_v = list(collections.OrderedDict().fromkeys(base_cols_p_v + headers).keys())
 pow_row_v = collections.namedtuple('woj_row', cols_p_v)
 
-res = process_gov(pow_vac_files, ["20210107"], {}, pow_row_v, -1, all_id)
+res = process_gov(pow_vac_files, ["20210107"], {}, pow_row_v, -1, ALL_ID)
 
-list2gzip(path_res_pow_vac, res, cols_p_v, len(cols_p_v))
+list2gzip(PATH_RES_POW_VAC, res, cols_p_v, len(cols_p_v))
 
 # Wojewodztwo Vac
-woj_vac_files = sorted(glob.glob('{path_data}/*_woj_szczepienia.csv'.format(path_data = path_data)))
-headers = get_plgov_cols(woj_vac_files, all_id)
-base_cols_p_v = [ 'wojewodztwo', 'liczba_szczepien_ogolem', 'liczba_szczepien_ogolnie', 'dawka_1_ogolem', 'dawka_2_ogolem', 'dawka_3_ogolem', 'dawka_1_dziennie', 'dawka_2_dziennie', 'dawka_3_dziennie', 'dawka_przypominajaca_dziennie', 'liczba_szczepien_dziennie', 'dawka_przypominajaca_ogolem', 'teryt', "name", "Date", "stan_rekordu_na"]
+woj_vac_files = sorted(glob.glob(f'{PATH_DATA}/*_woj_szczepienia.csv'))
+headers = get_plgov_cols(woj_vac_files, ALL_ID)
+base_cols_p_v = ['wojewodztwo', 'liczba_szczepien_ogolem', 'liczba_szczepien_ogolnie', 'dawka_1_ogolem',
+                 'dawka_2_ogolem', 'dawka_3_ogolem', 'dawka_1_dziennie', 'dawka_2_dziennie', 'dawka_3_dziennie',
+                 'dawka_przypominajaca_dziennie', 'liczba_szczepien_dziennie', 'dawka_przypominajaca_ogolem',
+                 'teryt', "name", "Date", "stan_rekordu_na"]
 cols_w_v = list(collections.OrderedDict().fromkeys(base_cols_p_v + headers).keys())
 woj_row_v = collections.namedtuple('woj_row', cols_w_v)
 
-res = process_gov(woj_vac_files, ["20210107"], {}, woj_row_v, -1, all_id)
+res = process_gov(woj_vac_files, ["20210107"], {}, woj_row_v, -1, ALL_ID)
 
-list2gzip(path_res_woj_vac, res, cols_w_v, len(cols_w_v))
+list2gzip(PATH_RES_WOJ_VAC, res, cols_w_v, len(cols_w_v))
--- gov/govDataUtils/funs.py
+++ gov/govDataUtils/funs.py
@@ -1,82 +1,143 @@
 # -*- coding: utf-8 -*-
+from collections import namedtuple
 import os
-import tempfile
 import urllib.request
-import zipfile
-import glob
 import gzip
-import csv
-import collections
 import datetime
 
-def list2gzip(path, data_list, cols, n_cols, encoding = "utf-8"):
-  assert isinstance(path, str)
-  assert isinstance(data_list, list)
-  assert isinstance(cols, list)
-  with gzip.open(path, 'w') as file:
-    file.write((",".join(cols[:(n_cols + 1)]) + "\n").encode(encoding))
-    file.writelines([(",".join(l[:(n_cols + 1)]) + "\n").encode(encoding) for l in data_list])
+def list2gzip(path: str, data_list: list, cols: list, n_cols: int, encoding: str = "utf-8") -> None:
+    """
+    write a gzip file from a list of data rows
+    :param path: a target path
+    :param data_list: a list of data rows
+    :param cols: n first columns to save
+    :param encoding: encoding type
+    """
+    assert isinstance(path, str)
+    assert isinstance(data_list, list)
+    assert isinstance(cols, list)
+    with gzip.open(path, 'w') as file:
+        file.write((",".join(cols[:(n_cols + 1)]) + "\n").encode(encoding))
+        file.writelines([(",".join(l[:(n_cols + 1)]) + "\n").encode(encoding) for l in data_list])
 
 def resolve_file_encoding(
-    bstring, 
-    look_w = "Cały",
-    base_encoding = "utf-8", 
-    alt_encoding = "Windows-1250",
-    lower = True
-  ):
-  assert isinstance(bstring, bytes), "bstring has to be a bytes"
-  assert isinstance(lower, bool), "lower has to be a bool"
-  try:
-    string_n = bstring.lower() if lower else bstring
-    look_w_n = look_w.lower() if lower else look_w
-    if string_n.decode(base_encoding) in look_w_n:
-      ee = base_encoding
-    else:
-      ee = alt_encoding
-  except:
-    ee = alt_encoding
-  return ee
+    bstring: str, 
+    look_w: str = "Cały",
+    base_encoding: str = "utf-8", 
+    alt_encoding: str = "Windows-1250",
+    lower: bool = True
+  ) -> str:
+    """
+    resolve a file encoding
+    :param bstring: a base text to check
+    :param look_w: a reference word
+    :param base_encoding: a base encoding
+    :param alt_encoding: an alternative encoding
+    :param lower: if to lower a base and reference words before the comparision.
+    """
+    assert isinstance(bstring, bytes), "bstring has to be a bytes"
+    assert isinstance(lower, bool), "lower has to be a bool"
+    try:
+        string_n = bstring.lower() if lower else bstring
+        look_w_n = look_w.lower() if lower else look_w
+        if string_n.decode(base_encoding) in look_w_n:
+            ee = base_encoding
+        else:
+            ee = alt_encoding
+    except:
+        ee = alt_encoding
+    return ee
 
-def get_plgov_cols(paths, bstring):
-  headers = list()
-  for file in paths:
-    with open(file, "rb") as csvfile:
-      csv_lines = csvfile.readlines()
-      row1 = csv_lines[1].strip().split(b";")
-      ee = resolve_file_encoding(row1[0], bstring)
-      header = [h.strip('"') for h in csv_lines[0].decode(ee).strip().split(";")]
-      headers = list(set(headers + header))
-  return headers
+def get_plgov_cols(paths: list[str], bstring: str) -> list[str]:
+    """
+    retrive unique values for files first row (columns names) 
+    :param paths: files paths
+    :param bstring: a word to resolve the encoding
+    """
+    headers = list()
+    for file in paths:
+        with open(file, "rb") as csvfile:
+            csv_lines = csvfile.readlines()
+            row1 = csv_lines[1].strip().split(b";")
+            ee = resolve_file_encoding(row1[0], bstring)
+            header = [h.strip('"') for h in csv_lines[0].decode(ee).strip().split(";")]
+            headers = list(set(headers + header))
+    return headers
 
-def process_gov(files, dates_to_skip, mapping, row_ntuple, bstring_pos, bstring):
-  res = list()
-  for file in files:
-    file_basename = os.path.basename(file)
-    file_date = datetime.date(int(file_basename[:4]), int(file_basename[4:6]), int(file_basename[6:8]))
-    stan_rekordu_na = str(file_date - datetime.timedelta(days=1))
-    file_date = str(file_date)
-    if file_basename[:8] in dates_to_skip:
-      continue
-    with open(file, "rb") as csvfile:
-      csv_lines = csvfile.readlines()
-      row1 = csv_lines[bstring_pos].strip().split(b";")
-      ee = resolve_file_encoding(row1[0], bstring)
-      header = [h.strip('"') for h in csv_lines[0].decode(ee).strip().split(";")]
-      for rowb in csv_lines[1:]:
-        row = [r.strip('"') for r in rowb.decode(ee).strip().split(";")]
-        row_dict = dict(zip(header, row))
-        row_dict["name"] = file_basename
-        row_dict["Date"] = file_date
-        row_dict["stan_rekordu_na"] = stan_rekordu_na
-        for k, v in mapping.items():
-          if k not in row_dict.keys():
-            row_dict[k] = row_dict[v]
-          if v not in row_dict.keys():
-            row_dict[v] = row_dict[k]
-        lacks = set(list(row_ntuple._fields)) - set(row_dict.keys())
-        if len(lacks) > 0:
-          for ll in lacks:
-            row_dict[ll] = ""
-        res_row = list(row_ntuple(**row_dict))
-        res.append(res_row)
-  return res
+def process_gov(files: list[str],
+                dates_to_skip: list[str],
+                mapping: dict, 
+                row_ntuple: namedtuple,
+                bstring_pos: int, 
+                bstring: str):
+    """
+    a function to process the gov.pl covid19 data
+    :param files: files to process
+    :param dates_to_skip: dates to omit
+    :param mapping: mapping with an old and new names
+    :param row_ntuple: one row of the data
+    :param bstring_pos: position in first data row to compare with a bstring
+    :param bstring_pos: a word which is used to indentify an encoding
+    """
+    res = []
+    for file in files:
+        file_basename = os.path.basename(file)
+        file_date = datetime.date(int(file_basename[:4]), int(file_basename[4:6]), int(file_basename[6:8]))
+        stan_rekordu_na = str(file_date - datetime.timedelta(days=1))
+        file_date = str(file_date)
+        if file_basename[:8] in dates_to_skip:
+            continue
+        with open(file, "rb") as csvfile:
+            csv_lines = csvfile.readlines()
+            row1 = csv_lines[bstring_pos].strip().split(b";")
+            ee = resolve_file_encoding(row1[0], bstring)
+            header = [h.strip('"') for h in csv_lines[0].decode(ee).strip().split(";")]
+            for rowb in csv_lines[1:]:
+                row = [r.strip('"') for r in rowb.decode(ee).strip().split(";")]
+                row_dict = dict(zip(header, row))
+                row_dict["name"] = file_basename
+                row_dict["Date"] = file_date
+                row_dict["stan_rekordu_na"] = stan_rekordu_na
+                for k, v in mapping.items():
+                    if k not in row_dict.keys():
+                        row_dict[k] = row_dict[v]
+                    if v not in row_dict.keys():
+                        row_dict[v] = row_dict[k]
+                lacks = set(list(row_ntuple._fields)) - set(row_dict.keys())
+                if len(lacks) > 0:
+                    for ll in lacks:
+                        row_dict[ll] = ""
+                res_row = list(row_ntuple(**row_dict))
+                res.append(res_row)
+    return res
+
+def retry(times, exceptions):
+    """
+    Retry Decorator
+    Retries the wrapped function/method `times` times if the exceptions listed
+    in ``exceptions`` are thrown
+    :param times: The number of times to repeat the wrapped function/method
+    :type times: Int
+    :param Exceptions: Lists of exceptions that trigger a retry attempt
+    :type Exceptions: Tuple of Exceptions
+    """
+    def decorator(func):
+        def newfn(*args, **kwargs):
+            attempt = 0
+            while attempt < times:
+                try:
+                    return func(*args, **kwargs)
+                except exceptions:
+                    print(f'Exception thrown when attempting to run {func}, attempt {attempt} of {times}')
+                    attempt += 1
+            return func(*args, **kwargs)
+        return newfn
+    return decorator
+
+@retry(times=3, exceptions=(ValueError, TypeError))
+def get_url(url: str) -> tuple:
+    """
+    a wrapper around urllib.request.urlretrieve with a 3 times retry
+    :param url: str a url to be download
+    """
+    return urllib.request.urlretrieve(url)
